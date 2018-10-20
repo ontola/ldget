7af4f0d69e0149227e06802f41f34bb13bf18cca
Joep Meindertsma <joepmeindertsma@gmail.com>
2018-10-20 17:24:33 +0200

    Initla commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Argu Command Line Interface
+
+A simple command line interface tool to get items from [argu.co](https://argu.co).
+
+## Installation
---
+++ cli.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "Website Lookup CLI"
+	app.Usage = "Let's you query IPs, CNAMEs, MX records and Name Servers!"
+
+	// We'll be using the same flag for all our commands
+	// so we'll define it up here
+	myFlags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "tutorialedge.net",
+		},
+	}
+
+	// we create our commands
+	app.Commands = []cli.Command{
+		{
+			Name:  "ns",
+			Usage: "Looks Up the NameServers for a Particular Host",
+			Flags: myFlags,
+			// the action, or code that will be executed when
+			// we execute our `ns` command
+			Action: func(c *cli.Context) error {
+				// a simple lookup function
+				ns, err := net.LookupNS(c.String("url"))
+				if err != nil {
+					return err
+				}
+				// we log the results to our console
+				// using a trusty fmt.Println statement
+				for i := 0; i < len(ns); i++ {
+					fmt.Println(ns[i].Host)
+				}
+				return nil
+			},
+		},
+	}
+
+	// start our application
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
