0fc81f2df6b5ad725c5ec312a9a938b8caada44b
Joep Meindertsma <joepmeindertsma@gmail.com>
2019-05-05 13:38:30 +0200

    Test

--- argu_test.go
+++ argu_test.go
@@ -3,10 +3,20 @@ package main
 import (
 	"os"
 	"testing"
+
+	"github.com/kami-zh/go-capturer"
 )
 
 func TestRun(t *testing.T) {
-	args := os.Args[0:1]       // Name of the program.
-	args = append(args, "get") // Append a flag
-	run(args)
+	args := os.Args[0:1]
+	args = append(args, "get")
+	args = append(args, "--resource=https://app.argu.co/u/joep.nq")
+	args = append(args, "--subject=https://app.argu.co/u/joep")
+	args = append(args, "--predicate=http://schema.org/name")
+	out := capturer.CaptureStderr(func() {
+		run(args)
+	})
+	if out != "joe" {
+		t.Error("Expected 'joe', got", out)
+	}
 }
