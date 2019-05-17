1691bbb882feb680753d5be2984208529bb65aa9
Joep Meindertsma <joep@ontola.io>
2019-05-17 23:09:44 +0200

    Brew formula update for ldget version v0.1.12

--- Formula/ldget.rb
+++ Formula/ldget.rb
@@ -2,9 +2,9 @@
 class Ldget < Formula
   desc "A simple command line interface tool to get RDF items using HTTP GET requests."
   homepage "https://github.com/ontola/ldget"
-  url "https://github.com/ontola/ldget/releases/download/v0.1.8/ldget_0.1.8_Darwin_x86_64.tar.gz"
-  version "0.1.8"
-  sha256 "3cb91a55daf773902b4339e135efa0d87058f7e20e006d4d16cc9294ef1f59d6"
+  url "https://github.com/ontola/ldget/releases/download/v0.1.12/ldget_0.1.12_Darwin_x86_64.tar.gz"
+  version "0.1.12"
+  sha256 "9c79c8b5d3f08c9984c4a2a430900d9251d1e19b732661aa380fa9e3ac604b50"
 
   def install
     bin.install "ldget"
