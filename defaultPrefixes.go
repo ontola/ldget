8c1b6394dc4de928277d88f7e97f16e7e79af3d7
Joep Meindertsma <joep@argu.co>
2020-01-24 10:40:36 +0100

    Move prefixes, don't log default creation

---
+++ defaultPrefixes.go
@@ -0,0 +1,64 @@
+package main
+
+var defaultPrefixes = []Prefix{
+	Prefix{
+		key: "schema",
+		url: "http://schema.org/",
+	},
+	Prefix{
+		key: "rdf",
+		url: "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+	},
+	Prefix{
+		key: "owl",
+		url: "http://www.w3.org/2002/07/owl#",
+	},
+	Prefix{
+		key: "ldp",
+		url: "http://www.w3.org/ns/ldp#",
+	},
+	Prefix{
+		key: "rdfa",
+		url: "http://www.w3.org/ns/rdfa#",
+	},
+	Prefix{
+		key: "rdfs",
+		url: "http://www.w3.org/2000/01/rdf-schema#",
+	},
+	Prefix{
+		key: "skos",
+		url: "http://www.w3.org/2004/02/skos/core#",
+	},
+	Prefix{
+		key: "vcard",
+		url: "http://www.w3.org/2006/vcard/ns#",
+	},
+	Prefix{
+		key: "xsd",
+		url: "http://www.w3.org/2001/XMLSchema#",
+	},
+	Prefix{
+		key: "dbp",
+		url: "http://dbpedia.org/",
+	},
+	Prefix{
+		key: "dbpr",
+		url: "http://dbpedia.org/resource/",
+	},
+	Prefix{
+		key: "dbpd",
+		url: "http://dbpedia.org/resource/",
+	},
+	Prefix{
+		key: "dbpp",
+		url: "http://dbpedia.org/property/",
+	},
+	Prefix{
+		key: "ncal",
+		url: "http://www.semanticdesktop.org/ontologies/2007/04/02/ncal#",
+	},
+	Prefix{
+		key: "org",
+		url: "http://www.w3.org/ns/org#",
+	},
+}
--- prefixes.go
+++ prefixes.go
@@ -11,69 +11,6 @@ import (
 	"strings"
 )
 
-var defaultPrefixes = []Prefix{
-	Prefix{
-		key: "schema",
-		url: "http://schema.org/",
-	},
-	Prefix{
-		key: "rdf",
-		url: "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-	},
-	Prefix{
-		key: "owl",
-		url: "http://www.w3.org/2002/07/owl#",
-	},
-	Prefix{
-		key: "ldp",
-		url: "http://www.w3.org/ns/ldp#",
-	},
-	Prefix{
-		key: "rdfa",
-		url: "http://www.w3.org/ns/rdfa#",
-	},
-	Prefix{
-		key: "rdfs",
-		url: "http://www.w3.org/2000/01/rdf-schema#",
-	},
-	Prefix{
-		key: "skos",
-		url: "http://www.w3.org/2004/02/skos/core#",
-	},
-	Prefix{
-		key: "vcard",
-		url: "http://www.w3.org/2006/vcard/ns#",
-	},
-	Prefix{
-		key: "xsd",
-		url: "http://www.w3.org/2001/XMLSchema#",
-	},
-	Prefix{
-		key: "dbp",
-		url: "http://dbpedia.org/",
-	},
-	Prefix{
-		key: "dbpr",
-		url: "http://dbpedia.org/resource/",
-	},
-	Prefix{
-		key: "dbpd",
-		url: "http://dbpedia.org/resource/",
-	},
-	Prefix{
-		key: "dbpp",
-		url: "http://dbpedia.org/property/",
-	},
-	Prefix{
-		key: "ncal",
-		url: "http://www.semanticdesktop.org/ontologies/2007/04/02/ncal#",
-	},
-	Prefix{
-		key: "org",
-		url: "http://www.w3.org/ns/org#",
-	},
-}
-
 // Prefix - A single combination of a key and URL
 type Prefix struct {
 	// The short version, e.g. "schema"
@@ -131,6 +68,8 @@ func readPrefixesFile(filePath string) []Prefix {
 	return prefixes
 }
 
+var colonCheck, _ = regexp.Compile(`(.*):(.*)`)
+
 // prefixToURL - Converts a prefix string to a full URI.
 // Returns the input string if no prefix is found.
 func prefixToURL(str string, prefixes []Prefix) string {
@@ -162,7 +101,6 @@ var selector, _ = regexp.Compile(`(.*)=(.*)`)
 
 // Creates file with default prefixes at default location
 func setDefaultPrefixes(filePath string) {
-	log.Printf("No prefixes file found, creating one at %v", filePath)
 	writePrefixes(filePath, defaultPrefixes)
 }
 
@@ -214,8 +152,6 @@ func tryURLToPrefix(url string, args Args) string {
 	}
 	return output
 }
-
-var colonCheck, _ = regexp.Compile(`(.*):(.*)`)
 
 // Returns URL for some prefix
 func getPrefix(key string, prefixes []Prefix) string {
